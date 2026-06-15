2d27572076c55858727c288878e52c54e076d0ab
Charles Rogers <charlesworthrogers@gmail.com>
2026-06-15 16:43:46 +0100

    Add research time background simulation

--- Source/TimeToBuild/Facilities/ResearchFacility.cs
+++ Source/TimeToBuild/Facilities/ResearchFacility.cs
@@ -1,35 +1,190 @@
 ﻿using UnityEngine;
 using KSP.UI.Screens;
 using KSP.Localization;
+using static TimeToBuild.MiscUtils;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace TimeToBuild.Facilities
 {
     public class ResearchFacility : WorkFacility
     {
+        private ProtoTechNode TechNodeToResearch = null;
+
         public ResearchFacility() : base(SpaceCenterFacility.ResearchAndDevelopment)
         {
 
         }
 
+        private void UnlockTech(string techID)
+        {
+            var protoTechNode = AssetBase.RnDTechTree.FindTech(techID);
+            ResearchAndDevelopment.Instance.UnlockProtoTechNode(protoTechNode);
+
+            if (!(RDController.Instance is null))
+            {
+                foreach (var rdNode in RDController.Instance.nodes)
+                {
+                    if (rdNode.tech.techID == techID)
+                    {
+                        rdNode.UpdateGraphics();
+                        break;
+                    }
+                }
+
+                RDController.Instance.UpdatePanel();
+                RDController.Instance.partList.Refresh();
+            }
+
+            var rdTech = GetMember<RDTech>(protoTechNode, "tech");
+
+            SpawnResearchCompleteDialog(techID);
+        }
+
         public override void OnWorkLoadComplete(WorkLoad workLoad)
         {
+            if (workLoad.WorkTech is null) return;
 
+            UnlockTech(workLoad.WorkTech.TechID);
         }
 
-        private void UnlockTech(RDTech tech)
+        public List<WorkChunk> ComputeResearchWorkChunks()
         {
-            tech.host.AddScience(-tech.scienceCost, TransactionReasons.RnDTechResearch);
-            tech.UnlockTech(!(tech.host is null));
+            var workChunks = new List<WorkChunk>();
+
+            if (UsingFacilities.Count == 0) return workChunks;
+
+            var constants = TimeToBuild.Instance.Calendar.GetTimeUnitVariables();
+            constants["cost"] = TechNodeToResearch.scienceCost;
+
+            foreach (var researchTime in TimeToBuild.Instance.Profile.ResearchTimes.Values)
+            {
+                if (!UsingFacilities.Contains(researchTime.Identifier.Facility)) continue;
+
+                var facilityVariables = GetFacilityVariables();
+                facilityVariables["facility_level"] = GetFacilityLevel(researchTime.Identifier.Facility);
+
+                var workChunk = new WorkChunk(researchTime.Identifier);
+                workChunk.Work = FormulaParser.ParseAndComputeFormula(researchTime.TimeFormula.Work, constants, facilityVariables);
+                workChunk.Overhead = FormulaParser.ParseAndComputeFormula(researchTime.TimeFormula.Overhead, constants, facilityVariables);
+
+                if (workChunk.Work > 0 || workChunk.Overhead > 0) workChunks.Add(workChunk);
+            }
+
+            return workChunks;
         }
 
-        private void StartResearch(RDNode rdNode)
+        public List<WorkVessel.WorkChunkDatum> GetWorkTechWorkChunkData()
         {
-            // Just unlock immediately for now
-            UnlockTech(rdNode.tech);
+            var workChunkData = new List<WorkVessel.WorkChunkDatum>();
 
-            rdNode.UpdateGraphics();
-            RDController.Instance.UpdatePanel();
-            RDController.Instance.partList.Refresh();
+            var workChunks = ComputeResearchWorkChunks();
+
+            var workRates = TimeToBuild.Instance.GetWorkRates();
+
+            foreach (var workChunk in workChunks)
+            {
+                if (!TimeToBuild.Instance.Profile.ResearchTimes.ContainsKey(workChunk.Identifier)) continue;
+
+                var researchTimeConfig = TimeToBuild.Instance.Profile.ResearchTimes[workChunk.Identifier];
+
+                if (workChunk.Work > 0 || workChunk.Overhead > 0)
+                {
+                    var workChunkDatum = new WorkVessel.WorkChunkDatum();
+                    workChunkDatum.Title = researchTimeConfig.Title;
+
+                    var rate = workRates[workChunk.Identifier];
+                    workChunkDatum.Duration = Convert.ToInt32(Math.Ceiling(workChunk.Work / rate + workChunk.Overhead));
+                    if (workChunkDatum.Duration < 0) workChunkDatum.Duration = 0;
+                    workChunkDatum.Duration = TimeToBuild.Instance.Calendar.RoundDuration(workChunkDatum.Duration);
+
+                    workChunkData.Add(workChunkDatum);
+                }
+            }
+
+            return workChunkData;
+        }
+
+        public void SpawnResearchDialog()
+        {
+            if (HighLogic.LoadedScene != GameScenes.SPACECENTER) return;
+
+            if (TechNodeToResearch is null) return;
+
+            var workChunkData = GetWorkTechWorkChunkData();
+
+            var title = "";
+            var totalResearchime = 0;
+            foreach (var workChunkDatum in workChunkData)
+            {
+                title += workChunkDatum.Title;
+
+                totalResearchime += workChunkDatum.Duration;
+
+                title += "\n" + TimeToBuild.Instance.Calendar.GetDurationString(workChunkDatum.Duration) + "\n\n";
+            }
+            title += LocalizerCache.Total + "\n" + TimeToBuild.Instance.Calendar.GetDurationString(totalResearchime) + "\n\n";
+
+            var completionDate = CurrentTime + totalResearchime;
+
+            var message = "";
+            foreach (var date in TimeToBuild.Instance.GetSalientDates(completionDate)) message += TimeToBuild.Instance.Calendar.GetDateString(date.Item1) + " — " + date.Item2 + "\n";
+
+            var optionStartResearch = GetBuildDialogButton(LocalizerCache.StartResearch, OnStartResearch);
+
+            SpawnMultiOptionDialog(title, message, optionStartResearch);
+        }
+
+        private bool TryStartResearch(List<WorkChunk> workChunks, bool actuallyAddIt)
+        {
+            if (HighLogic.LoadedScene != GameScenes.SPACECENTER) return false;
+
+            var workTech = new WorkTech(TechNodeToResearch.techID);
+            var workLoad = new WorkLoad(CurrentTime, workChunks, workTech);
+            var success = TryAddWorkLoad(workLoad, actuallyAddIt);
+
+            if (!success) SpawnMultiOptionDialog(LocalizerCache.CannotStartResearch, LocalizerCache.ResearchFacilityBusy);
+
+            return success;
+        }
+
+        public static string GetTechTitle(string techID)
+        {
+            ConfigNode techNode = GameDatabase.Instance.GetConfigNodes("RDNode").FirstOrDefault(t => t.GetValue("id") == techID);
+
+            return techNode is null ? techID : techNode.GetValue("title");
+        }
+
+        public void OnStartResearch()
+        {
+            if (TechNodeToResearch is null) return;
+
+            var workChunks = ComputeResearchWorkChunks();
+
+            var workChunkData = GetWorkTechWorkChunkData();
+
+            var completionTime = CurrentTime;
+            foreach (var workChunkDatum in workChunkData) completionTime += workChunkDatum.Duration;
+            
+            if (TryStartResearch(workChunks, true))
+            {
+                ResearchAndDevelopment.Instance.AddScience(-TechNodeToResearch.scienceCost, TransactionReasons.RnDTechResearch);
+
+                var techTitle = GetTechTitle(TechNodeToResearch.techID);
+
+                var alarm = new AlarmTypeRaw();
+                alarm.ut = completionTime;
+                alarm.title = techTitle + " " + LocalizerCache.ResearchCompleteTitle;
+                alarm.description = techTitle + " " + LocalizerCache.ResearchCompleteDescription;
+
+                AlarmClockScenario.Instance.alarms.Add((uint)new System.Random().Next(), alarm);
+            }
+        }
+
+        public void SpawnResearchCompleteDialog(string techID)
+        {
+            SpawnMultiOptionDialog(LocalizerCache.ResearchComplete, GetTechTitle(techID) + " " + LocalizerCache.NowAvailable);
         }
 
         public RDTech.OperationResult TryStartResearch(RDNode rdNode)
@@ -59,9 +214,10 @@ namespace TimeToBuild.Facilities
 
                 operationResult = RDTech.OperationResult.ScienceCostLimitExceeded;
             }
-            else
+            else if (!(RDController.Instance is null))
             {
-                StartResearch(rdNode);
+                TechNodeToResearch = AssetBase.RnDTechTree.FindTech(rdNode.tech.techID);
+                SpawnResearchDialog();
             }
 
             GameEvents.OnTechnologyResearched.Fire(new GameEvents.HostTargetAction<RDTech, RDTech.OperationResult>(rdNode.tech, operationResult));
